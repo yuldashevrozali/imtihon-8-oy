bf0e93f44be58489116d3ededcb1d0bade478bf7
Yuldashevrozali <Yuldashevrozali08@gmail.com>
2024-04-20 03:28:51 +0500

    user

--- src/components/Currency/index.jsx
+++ src/components/Currency/index.jsx
@@ -44,93 +44,121 @@ export default function Currency() {
         }
     }
 
+    const handleCoinClick = (coin) => {
+        navigate(`/crypto/${coin.id}`);
+        console.log(49, coin);
+
+        const coinInfo = {
+            coinimg: coin.image,
+            coinprice: coin.current_price
+        };
+
+        let coinsData = localStorage.getItem("coins");
+        let parsedCoinsData = coinsData ? JSON.parse(coinsData) : {};
+
+        if (!parsedCoinsData.cryptos) {
+            parsedCoinsData.cryptos = [];
+        }
+
+        parsedCoinsData.cryptos.push(coinInfo);
+
+        localStorage.setItem("coins", JSON.stringify(parsedCoinsData));
+    };
+
+
+
+
     return (
         <>
-            {data ? (
-                <div className="header-bottom">
-                    <div className='hello'>
-                        <h1>CRYPTOFOLIO WATCH LIST</h1>
+            {!data ? (
+                <div>Loading...</div>
+            ) : (
+                <div>
+                    <div className="header-bottom">
+                        <div className='hello'>
+                            <h1>CRYPTOFOLIO WATCH LIST</h1>
+                        </div>
+
+                        <p>Get all the Info regarding your favorite Crypto Currency</p>
+
+                        <Swiper
+                            modules={[Autoplay]}
+                            spaceBetween={50}
+                            slidesPerView={3}
+                            autoplay={{ delay: 2000, disableOnInteraction: true }}
+                            loop={true}
+                        >
+                            {data.map((coin) => (
+                                <SwiperSlide key={coin.id}>
+                                    <div onClick={() => navigate(`/crypto/${coin.id}`)} className='sliders'>
+                                        <img src={coin.image} alt={coin.name} />
+                                        <div className='sliders-flex'>
+                                            <h2>{coin.symbol.toUpperCase()}</h2>
+
+                                            <p style={{ color: coin.price_change_percentage_24h_in_currency < 0 ? 'red' : '#0ECB81' }}>
+                                                {coin.price_change_percentage_24h_in_currency.toFixed(1)}%
+                                            </p>
+                                        </div>
+
+                                        <h1>{selectedOption === 'USD' ? '$' : selectedOption === 'INR' ? '₹' : '₽'}{coin.current_price}</h1>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+
                     </div>
 
-                    <p>Get all the Info regarding your favorite Crypto Currency</p>
-
-                    <Swiper
-                        modules={[Autoplay]}
-                        spaceBetween={50}
-                        slidesPerView={3}
-                        autoplay={{ delay: 2000, disableOnInteraction: true }}
-                        loop={true}
-                    >
-                        {data.map((coin) => (
-                            <SwiperSlide key={coin.id}>
-                                <div onClick={() => navigate(`/crypto/${coin.id}`)}  className='sliders'>
-                                    <img src={coin.image} alt={coin.name} />
-                                    <div className='sliders-flex'>
-                                    <h2>{coin.symbol.toUpperCase()}</h2>
-
-                                        <p style={{ color: coin.price_change_percentage_24h_in_currency < 0 ? 'red' : '#0ECB81' }}>
-                                            {coin.price_change_percentage_24h_in_currency.toFixed(1)}%
-                                        </p>
+                    <div className="currency">
+                        <div className="currency-flex">
+                            <h1>Cryptocurrency Prices by Market Cap</h1>
+                            <div className="currency-input">
+                                <input type="text" placeholder='Search For a Crypto Currency..' />
+                            </div>
+                            <div>
+                                <div className="table-top">
+                                    <p id='coin' >Coin</p>
+                                    <div className="table-flex" >
+                                        <p>Price</p>
+                                        <p>24h Change</p>
+                                        <p>Market Cap</p>
                                     </div>
-
-                                    <h1>{selectedOption === 'USD' ? '$' : selectedOption === 'INR' ? '₹' : '₽'}{coin.current_price}</h1>
                                 </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
+                                {data && data.map((coin) => (
+                                    <div key={coin.id} onClick={() => handleCoinClick(coin)} className="table-bottom">
+                                        <img src={coin.image} alt="coin.image" />
+                                        <div className="table-coin">
+                                            <h1>{coin.symbol}</h1>
+                                            <p>{coin.name}</p>
+                                        </div>
+                                        <div className="table-prices">
+                                            <p id="coin-price">{selectedOption === 'USD' ? '$' : selectedOption === 'INR' ? '₹' : '₽'}{coin.current_price}</p>
+                                            <p className={coin.price_change_percentage_24h_in_currency.toString().startsWith('-') ? 'red' : 'blue'}>
+                                                <img src={eye} alt="" />
+                                                {coin.price_change_percentage_24h_in_currency.toFixed(1)}%
+                                            </p>
+                                            <p id="coin-price1" >{formatMarketCap(coin.market_cap)}</p>
 
+                                        </div>
+                                    </div>
+                                ))}
+                                <div className='next-slide'>
+                                    <ul>
+                                        <li><img src={left} alt="" /></li>
+                                        {[...Array(10)].map((_, index) => (
+                                            <li key={index} onClick={() => setPage(index + 1)}>{index + 1}</li>
+                                        ))}
+                                        <li><img src={right} alt="" /></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
-            ) : (
-                <div>Loading...</div>
+
+
             )}
 
-
-            <div className="currency">
-                <div className="currency-flex">
-                    <h1>Cryptocurrency Prices by Market Cap</h1>
-                    <div className="currency-input">
-                        <input type="text" placeholder='Search For a Crypto Currency..' />
-                    </div>
-                    <div>
-                        <div className="table-top">
-                            <p id='coin' >Coin</p>
-                            <div className="table-flex" >
-                                <p>Price</p>
-                                <p>24h Change</p>
-                                <p>Market Cap</p>
-                            </div>
-                        </div>
-                        {data && data.map((coin) => (
-                            <div key={coin.id} onClick={() => navigate(`/crypto/${coin.id}`)} className="table-bottom">
-                                <img src={coin.image} alt="coin.image" />
-                                <div className="table-coin">
-                                    <h1>{coin.symbol}</h1>
-                                    <p>{coin.name}</p>
-                                </div>
-                                <div className="table-prices">
-                                    <p id="coin-price">{selectedOption === 'USD' ? '$' : selectedOption === 'INR' ? '₹' : '₽'}{coin.current_price}</p>
-                                    <p className={coin.price_change_percentage_24h_in_currency.toString().startsWith('-') ? 'red' : 'blue'}>
-                                        <img src={eye} alt="" />
-                                        {coin.price_change_percentage_24h_in_currency.toFixed(1)}%
-                                    </p>
-                                    <p id="coin-price1" >{formatMarketCap(coin.market_cap)}</p>
-
-                                </div>
-                            </div>
-                        ))}
-                        <div className='next-slide'>
-                            <ul>
-                                <li><img src={left} alt="" /></li>
-                                {[...Array(10)].map((_, index) => (
-                                    <li key={index} onClick={() => setPage(index + 1)}>{index + 1}</li>
-                                ))}
-                                <li><img src={right} alt="" /></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
 
         </>
 
--- src/components/Headers/index.jsx
+++ src/components/Headers/index.jsx
@@ -1,15 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useRef, useEffect } from 'react';
 import dropdown from '../../assets/dropdown.svg';
 import './index.css';
-import { Link } from 'react-router-dom';
 import { SelectedOptionContext } from '../Context/context';
 
 export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const modalRef = useRef(null);
     const { selectedOption, setSelectedOption } = useContext(SelectedOptionContext);
+    const [data, setdata] = useState([]);
 
     const handleSelect = (option) => {
         setSelectedOption(option);
     };
+    const toggleModal = () => {
+        setIsOpen(!isOpen);
+        const coins = localStorage.getItem('coins');
+        const parsedCoins = coins ? JSON.parse(coins) : null;
+        setdata(parsedCoins.cryptos);
+    };
+
+
+    const handleCloseModal = () => {
+        setIsOpen(false);
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleEscKey);
+
+        return () => {
+            document.removeEventListener('keydown', handleEscKey);
+        };
+    }, []);
+
+    const handleEscKey = (e) => {
+        if (e.key === 'Escape') {
+            setIsOpen(false);
+        }
+    };
+
+    function removeclick(index) {
+        setdata(prevData => {
+            const newData = [...prevData];
+            newData.splice(index, 1);
+            localStorage.setItem('coins', JSON.stringify({ cryptos: newData }));
+            return newData;
+        });
+    }
+
+
 
     return (
         <>
@@ -22,13 +60,30 @@ export default function Header() {
                         <button className="dropbtn">{selectedOption}<span><img src={dropdown} alt="dropdown" /></span></button>
                         <div className="dropdown-content">
                             <a onClick={() => handleSelect('USD')}>USD</a>
-                            <a onClick={() => handleSelect('INR')}>INR</a> 
-                            <a onClick={() => handleSelect('RUB')}>RUB</a> 
+                            <a onClick={() => handleSelect('INR')}>INR</a>
+                            <a onClick={() => handleSelect('RUB')}>RUB</a>
                         </div>
                     </div>
-                    <Link to={`/${selectedOption}`} className='watchlistbtn'>watchlist</Link>
+                    <button onClick={toggleModal} className='watchlistbtn'>WATCH LIST</button>
                 </div>
             </div>
+            {isOpen && (
+                <div className="modal-container" onClick={handleCloseModal}>
+                    <div className="modal-content"  ref={modalRef}>
+                        <h2>WATCHLIST</h2>
+                        <div className='coins-watchlist'>
+                            {data.map((coin, index) => (
+                                <div className='coin-block' key={index}>
+                                    <img src={coin.coinimg} alt="" />
+                                    <h1>{coin.coinprice.toLocaleString()}</h1>
+                                    <button onClick={() => removeclick(index)}>remove</button>
+
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
